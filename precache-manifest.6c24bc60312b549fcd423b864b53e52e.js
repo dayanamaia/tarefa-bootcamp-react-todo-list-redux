3309fc809c052177f737c502615bfe9839809e5d
Dayana Maia <dayrmaia@gmail.com>
2020-04-05 18:04:35 -0300

    Updates

---
+++ precache-manifest.6c24bc60312b549fcd423b864b53e52e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7670fb0c245411bbad80538f104aed5f",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/index.html"
+  },
+  {
+    "revision": "0fe4c5834a40eff85bd5",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "d06fe621e35470a7cec1",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/static/css/main.b9caf51e.chunk.css"
+  },
+  {
+    "revision": "0fe4c5834a40eff85bd5",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/static/js/2.60aebda6.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/static/js/2.60aebda6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06fe621e35470a7cec1",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/static/js/main.2fb6ada6.chunk.js"
+  },
+  {
+    "revision": "7998c0180365e6a49316",
+    "url": "/tarefa-bootcamp-react-todo-list-redux/static/js/runtime-main.8b7e5590.js"
+  }
+]);
